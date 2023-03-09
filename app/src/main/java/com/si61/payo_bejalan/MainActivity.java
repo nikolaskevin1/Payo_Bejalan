8e3c7d75478f2afccc9400758712671d6478da60
kevin10042001 <104200445+kevin10042001@users.noreply.github.com>
2023-03-09 18:32:23 +0700

    2

--- app/src/main/java/com/si61/payo_bejalan/MainActivity.java
+++ app/src/main/java/com/si61/payo_bejalan/MainActivity.java
@@ -1,14 +1,32 @@
 package com.si61.payo_bejalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fabTambah;
+    private RecyclerView rvDestinasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabTambah = findViewById(R.id.fab_tambah);
+        rvDestinasi = findViewById(R.id.rv_destinasi);
+
+        fabTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,TambahActivity.class));
+            }
+        });
+
     }
 }
